58d8d2d9beb1ad526207b9d52b2659033d9b53c8
mashrafisarkar02 <mashrafisarkarefty@gmail.com>
2026-09-20 16:08:27 +0600

    added two problem

---
+++ problem-1.js
@@ -0,0 +1,5 @@
+// ********* Divide the Asset *************//
+
+var area = 800;
+var divideAsset = area / 2;
+console.log(divideAsset);
---
+++ problem-2.js
@@ -0,0 +1,11 @@
+// **************** Cycle or Laptop ************
+
+var money = 6000;
+if (money >= 25000){
+    console.log('Laptop');
+} else if( money >= 10000){
+    console.log('Cycle');
+} 
+else{
+    console.log('Chocolate')
+}
